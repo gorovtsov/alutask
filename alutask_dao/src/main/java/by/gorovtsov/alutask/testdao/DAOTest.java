d0fe3fa44805018e1cebe2a0b108a6822ba92ef7
Alexander Gorovtsov <garavek94@gmail.com>
2018-01-17 21:24:56 +0300

    Checkstyle added

--- alutask_dao/src/main/java/by/gorovtsov/alutask/testdao/DAOTest.java
+++ alutask_dao/src/main/java/by/gorovtsov/alutask/testdao/DAOTest.java
@@ -1,7 +1,7 @@
 package by.gorovtsov.alutask.testdao;
 
 public class DAOTest {
-    public String getStringValue(){
-        return "Hello from DAO!!! You're cool!";
+    public String getStringValue() {
+        return "Hello from DAO!!! You're cool if you are Here!";
     }
 }
